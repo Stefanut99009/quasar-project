a278c3c6ddc3c2e66b3a5a09cf1ffb62f9a215f4
Stefanut99009 <stefanutpazargic@gmail.com>
2024-12-02 20:54:16 +0200

    first commit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,18 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <div class="row">
+      <div class="col-12 col-md-4 col-lg-4">
+        <div class="text-h4 text-bold">View Blog</div>
+        <div class="text-grey-7">Please login your account below</div>
+        <div class="column">
+          <q-input
+          v-model="form.email.value"
+          :error="form.email.error"
+          :error-message="form.email.msg">
+          </q-input>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
